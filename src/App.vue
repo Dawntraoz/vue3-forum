4480f01c947b1730f128782fbfb9a9db48dc61ca
Dawntraoz <albasilventefu@gmail.com>
2021-03-28 01:43:35 +0100

    Chapter 1 & 2

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <header></header>
+  <div class="container">
+    <page-home />
+  </div>
+  <footer></footer>
+</template>
+
+<script>
+import PageHome from '@/components/PageHome.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PageHome
+  }
+}
+</script>
+
+<style>
+@import "assets/css/styles.css";
+</style>
